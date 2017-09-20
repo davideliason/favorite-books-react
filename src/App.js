d4000b7fdd76a278cffb8451d3bc8aa1f053b28a
davideliason <davethemaker@gmail.com>
2017-09-20 09:07:56 -0700

    create-react-app with sample JSON data ready to go

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+var book-data = [
+  {category : "science fiction", author: "Isaac Asimov",title:"I, Robot",owned:false},
+  {category : "science fiction", author: "Frank Herbert", title: "Dune", owned: false},
+  {category : "mystery", author: "Sue Grafton", title: "A is for Alibi", owned: false}
+  {category : "mystery", author: "Dan Brown", title: "The Da Vinci Code",owned: false},
+  {category : "self-help", author: "Thich Nhat Hanh", title:  "Being", owned: true}
+];
+
+export default App;
